fcc17e878d5c7505061c4ff5cfe65e8235dd2bfb
Ravi Dudhatra <ravidudhatrawork@gmail.com>
2024-07-31 14:09:18 +0530

    Removed all comments and reviewed the entire project to ensure everything works correctly.

--- StudentSync.WebApi/Controllers/ProfileApiController.cs
+++ StudentSync.WebApi/Controllers/ProfileApiController.cs
@@ -1,32 +1,4 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using StudentSync.Core.Services.Interface;
-
-//namespace StudentSync.WebApi.Controllers
-//{
-//    [Route("api/ProfileApiController")]
-//    [ApiController]
-//    public class ProfileApiController : ControllerBase
-//    {
-//        private readonly IProfileService _profileService;
-
-//        public ProfileApiController(IProfileService profileService)
-//        {
-//            _profileService = profileService;
-//        }
-
-//        [HttpGet("get-profile/{username}")]
-//        public async Task<IActionResult> GetProfile(string username)
-//        {
-//            var profile = await _profileService.GetProfileAsync(username);
-//            if (profile == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(profile);
-//        }
-//    }
-//}
+﻿
 
 
 using Microsoft.AspNetCore.Mvc;
@@ -61,25 +33,6 @@ namespace StudentSync.WebApi.Controllers
             return Ok(profile);
         }
 
-        [HttpPost("update-profile")]
-        public async Task<IActionResult> UpdateProfile([FromForm] ProfileViewModel model)
-        {
-            if (model.ImageFile != null)
-            {
-                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
-                string uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImageFile.FileName;
-                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    await model.ImageFile.CopyToAsync(fileStream);
-                }
-
-                model.ProfileImage = "/images/" + uniqueFileName;
-            }
-
-            await _profileService.UpdateProfileAsync(model);
-            return Ok();
-        }
+       
     }
 }
